a15cf473d52c25d963e1ac0212514623b64630d1
tetuomi <kondo.tetta851@mail.kyutech.jp>
2020-11-17 20:37:18 +0900

    Change font color

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -179,7 +179,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#222" :foreground "color-231" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 1 :width normal :foundry "default" :family "default"))))
+ '(font-lock-string-face ((t (:foreground "brightcyan")))))
 
 ;;current directory 表示
 (let ((ls (member 'mode-line-buffer-identification
